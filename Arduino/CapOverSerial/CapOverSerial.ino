85ce216e321a3a609c6ade3076f3564777e353fe
OwenDaughtery <OwenJ.daughtery@gmail.com>
2016-04-06 21:57:38 +0100

    secondary pin
    added functionality of a secondary pin, also second graph plotter on
    arduino. Now waiting on 10m ohm resistors to free up board.

--- Arduino/CapOverSerial/CapOverSerial.ino
+++ Arduino/CapOverSerial/CapOverSerial.ino
@@ -1,25 +1,38 @@
 #include <CapacitiveSensor.h>
 
-CapacitiveSensor cs = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor cs_14_15 = CapacitiveSensor(14,15);        // 10M resistor between pins 14 & 15, pin 15 is sensor pin, add a wire and or foil if desired
 
 long last = 0;
-float cof = 0.25;
+float cof = 0.3;
 
 void setup(){
-   cs.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    Serial.begin(9600);  
 }
 
 void loop(){
     long start = millis();
-    long total =  cs.capacitiveSensor(30);
+    long total =  cs_4_2.capacitiveSensor(30);
     long temp = last + cof * (total - last);
+        
+    long totaltest = cs_14_15.capacitiveSensor(30); //is a test
+
     
+    //Serial.print("Test Commencing");
+    //Serial.print(totaltest);
+    //Serial.print("\t"); 
+    //Serial.println("");
+    //Serial.print("Test Ending");    
 
     delay(10);                             // arbitrary delay to limit data to serial port 
-    Serial.print(temp);
+    Serial.print(totaltest);
+    Serial.print(" ");
+    Serial.println(temp);
+    //Serial.print(" ");
+    //Serial.print(50);
     Serial.print("\t"); 
-    Serial.println("");
+    Serial.print("");
 
     last = temp;
     
