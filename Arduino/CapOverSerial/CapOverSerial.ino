1c64c15aca3278da452409d917ea21a62d67be49
OwenDaughtery <OwenJ.daughtery@gmail.com>
2016-04-11 13:10:14 +0100

    Threshold update
    commented out threshold updates

--- Arduino/CapOverSerial/CapOverSerial.ino
+++ Arduino/CapOverSerial/CapOverSerial.ino
@@ -5,6 +5,7 @@ CapacitiveSensor cs_8_6 = CapacitiveSensor(8,6);        // 10M resistor between 
 
 long last1 = 0;
 long last2 = 0;
+long thresh = 0;
 
 float cof = 0.3;
 
@@ -15,10 +16,17 @@ void setup(){
 
 void loop(){
     long start = millis();
+
     
     long total1 =  cs_4_2.capacitiveSensor(30);
     long temp1 = last1 + cof * (total1 - last1);
-        
+
+// begin threshold test
+//    if (thresh == 0)
+//      thresh = (total1 * 2);
+//    long threshtotal = total1 - thresh;
+//  end threshold test
+ 
     long total2 = cs_8_6.capacitiveSensor(30);
     long temp2 = last2 + cof * (total2 - last2);
    
@@ -27,10 +35,13 @@ void loop(){
     Serial.print(temp1);
     Serial.print("\t");
     Serial.println(temp2);
-    Serial.print("\t"); 
+    Serial.print("\t");
     Serial.print("");
 
     last1 = temp1;
     last2 = temp2;
     
 }
+
+//thereshold is subtractor from values, inverse square law 1\r2, logarthmic, swaure root it, have cooeficient, foil ground plane, grounding of building
+
