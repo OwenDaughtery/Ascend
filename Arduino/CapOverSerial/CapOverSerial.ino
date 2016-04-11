a69548d6145da484ed7983c64cee184053b30c99
Paul Hayes <paul.hayes@entropedia.co.uk>
2016-04-11 17:24:34 +0100

    Added scale and threshold calibration

--- Arduino/CapOverSerial/CapOverSerial.ino
+++ Arduino/CapOverSerial/CapOverSerial.ino
@@ -1,4 +1,10 @@
 #include <CapacitiveSensor.h>
+#include <Encoder.h>
+#include <elapsedMillis.h>
+#include <Bounce2.h>
+
+#define minScale 1
+#define maxScale 255
 
 CapacitiveSensor cs[8] = {
   CapacitiveSensor(5, 6), // 10M resistor between pins 5 & 6, pin 6 is sensor pin, add a wire and or foil if desired
@@ -11,34 +17,76 @@ CapacitiveSensor cs[8] = {
   CapacitiveSensor(5, 16)
 };
 
+Encoder dial(3,4);
+Bounce debouncer = Bounce(); 
+
+elapsedMillis timeElapsed;
+
 long lasts[8] = {
   0,0,0,0,0,0,0,0  
 };
 
+long thresholds[8] = {
+  0,0,0,0,0,0,0,0
+};
+
+float thresholdMargin = 1.1;
+long scale;
 
 float cof = 0.3;
 
 void setup(){
+
+  pinMode(2,INPUT_PULLUP);
+  pinMode(3,INPUT_PULLUP);
+  pinMode(4,INPUT_PULLUP);
+  
+  debouncer.attach(2);
+  debouncer.interval(10);
+  
   for(int i=0; i<8; i++){
      cs[i].set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    }
+   
    Serial.begin(9600);  
 }
 
 void loop(){
-    long start = millis();
+    debouncer.update();
+    timeElapsed = 0;
+    if( debouncer.fell() ){
+      for(int i=0; i<8; i++){
+        thresholds[i] = lasts[i] * thresholdMargin;
+      }
+      
+    }
+
+    scale = dial.read() / 48;
+    if( scale < minScale ){
+      dial.write(scale=48*minScale);
+    }
+    if( scale > maxScale ){
+      dial.write(scale=48*maxScale);
+    }
+
 
   for(int i=0; i<8; i++){
     long total =  cs[i].capacitiveSensor(30);
     long temp = lasts[i] + cof * (total - lasts[i]);   
+    long adjustedVal = scale * sqrt( temp-thresholds[i] );
     if (i!=0)
       Serial.print(",");
-    Serial.print(temp);  
+    Serial.print( adjustedVal );  
     lasts[i] = temp;
    }
    Serial.println("");
 
-    delay(10);                             // arbitrary delay to limit data to serial port     
+
+
+    long delayDuration = 40-timeElapsed;
+    if( delayDuration > 0 ){
+      delay(delayDuration);                             // arbitrary delay to limit data to serial port     
+    }
 
 }
 
