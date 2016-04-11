a368c09ef1d4f231220b86b4cf07e91036a8dc0f
OwenDaughtery <OwenJ.daughtery@gmail.com>
2016-04-11 13:30:23 +0100

    array update
    Added all 8 sensor compatibility along with arrays, and for loops to
    simplify code.

--- Arduino/CapOverSerial/CapOverSerial.ino
+++ Arduino/CapOverSerial/CapOverSerial.ino
@@ -1,46 +1,45 @@
 #include <CapacitiveSensor.h>
 
-CapacitiveSensor cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
-CapacitiveSensor cs_8_6 = CapacitiveSensor(8,6);        // 10M resistor between pins 8 & 6, pin 6 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor cs[8] = {
+  CapacitiveSensor(5, 6), // 10M resistor between pins 5 & 6, pin 6 is sensor pin, add a wire and or foil if desired
+  CapacitiveSensor(5, 7),
+  CapacitiveSensor(5, 8),
+  CapacitiveSensor(5, 9),
+  CapacitiveSensor(5, 10),
+  CapacitiveSensor(5, 14),
+  CapacitiveSensor(5, 15),
+  CapacitiveSensor(5, 16)
+};
 
-long last1 = 0;
-long last2 = 0;
-long thresh = 0;
+long lasts[8] = {
+  0,0,0,0,0,0,0,0  
+};
+
 
 float cof = 0.3;
 
 void setup(){
-   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+  for(int i=0; i<8; i++){
+     cs[i].set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   }
    Serial.begin(9600);  
 }
 
 void loop(){
     long start = millis();
 
-    
-    long total1 =  cs_4_2.capacitiveSensor(30);
-    long temp1 = last1 + cof * (total1 - last1);
+  for(int i=0; i<8; i++){
+    long total =  cs[i].capacitiveSensor(30);
+    long temp = lasts[i] + cof * (total - lasts[i]);   
+    if (i!=0)
+      Serial.print(",");
+    Serial.print(temp);  
+    lasts[i] = temp;
+   }
+   Serial.println("");
 
-// begin threshold test
-//    if (thresh == 0)
-//      thresh = (total1 * 2);
-//    long threshtotal = total1 - thresh;
-//  end threshold test
- 
-    long total2 = cs_8_6.capacitiveSensor(30);
-    long temp2 = last2 + cof * (total2 - last2);
-   
+    delay(10);                             // arbitrary delay to limit data to serial port     
 
-    delay(10);                             // arbitrary delay to limit data to serial port 
-    Serial.print(temp1);
-    Serial.print("\t");
-    Serial.println(temp2);
-    Serial.print("\t");
-    Serial.print("");
-
-    last1 = temp1;
-    last2 = temp2;
-    
 }
 
 //thereshold is subtractor from values, inverse square law 1\r2, logarthmic, swaure root it, have cooeficient, foil ground plane, grounding of building
