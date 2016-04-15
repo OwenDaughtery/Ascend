6335fb04d9ab5407eb56fbcb590602f3dc910a9c
OwenDaughtery <OwenJ.daughtery@gmail.com>
2016-04-15 13:07:50 +0100

    sin wave update
    Messed around with different types of wires and copper, added a few
    blocks of code.

--- Arduino/CapOverSerial/CapOverSerial.ino
+++ Arduino/CapOverSerial/CapOverSerial.ino
@@ -5,6 +5,7 @@
 
 #define minScale 1
 #define maxScale 255
+#define raw 0
 
 CapacitiveSensor cs[8] = {
   CapacitiveSensor(5, 6), // 10M resistor between pins 5 & 6, pin 6 is sensor pin, add a wire and or foil if desired
@@ -30,7 +31,10 @@ long thresholds[8] = {
   0,0,0,0,0,0,0,0
 };
 
-float thresholdMargin = 1.3;
+int pins[8] = {
+  6, 7, 8, 9, 10, 14, 15, 16
+};
+float thresholdMargin = 1.5;
 long scale;
 
 float cof = 0.4;
@@ -77,20 +81,34 @@ void loop(){
 
   for(int i=0; i<8; i++){
     long total =  cs[i].capacitiveSensor(30);
+    //long total = 0;
     long temp = lasts[i] + cof * (total - lasts[i]);   
     long adjustedVal = scale * sqrt(max(temp-thresholds[i],0));
     if (i!=0)
       Serial.print(",");
-    Serial.print(min(255, adjustedVal ));  
+      #if raw
+        Serial.print(total);
+      #else      
+        Serial.print(min(255, adjustedVal ));  
+      #endif
     lasts[i] = temp;
    }
    Serial.println("");
 
-    long delayDuration = 40-timeElapsed;
+    /*long delayDuration = 40-timeElapsed;
     if( delayDuration > 0 ){
       delay(delayDuration);                             // arbitrary delay to limit data to serial port     
-    }
+    }*/
 
+    for(int i=0; i<8; i++){
+      pinMode(pins[i], OUTPUT);
+      digitalWrite(pins[i], LOW);
+      }
+    delay(5);
+    for(int i=0; i<8; i++){
+      pinMode(pins[i], INPUT);
+      }
+      
 }
 
 //thereshold is subtractor from values, inverse square law 1\r2, logarthmic, swaure root it, have cooeficient, foil ground plane, grounding of building
